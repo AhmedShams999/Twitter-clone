f6b12bc69f0b498419d90c4bae79808d4ff72417
Ahmed <Ahmedshams22@gmail.com>
2024-05-02 00:35:40 +0300

    1- user routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 import express from "express"
-import authRoutes from "./routes/auth.routes.js"
+import {v2 as cloudinary} from 'cloudinary';
 import dotenv from "dotenv" 
-import connetMongoDB from "./db/connectToMongo.js";
 import cookieParser from "cookie-parser";
 
+
+import authRoutes from "./routes/auth.routes.js"
+import usershRoutes from "./routes/user.routes.js"
+import connetMongoDB from "./db/connectToMongo.js";
+
 dotenv.config()
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_CLOUD_API_KEY,
+  api_secret: process.env.CLOUDINARY_CLOUD_API_SECRET
+})
 
 const app = express(); 
 const PORT = process.env.PORT || 5000
@@ -13,6 +22,7 @@ app.use(express.urlencoded({extended:true})) // to accepte the urlencoded type
 app.use(cookieParser())
 
 app.use("/api/auth",authRoutes)
+app.use("/api/users",usershRoutes)
 
 
 app.listen(PORT,()=>{
