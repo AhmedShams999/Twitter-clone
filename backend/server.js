9d48d105deda5f8934309bde83790dc80198d3e9
AhmedShams999 <106814036+AhmedShams999@users.noreply.github.com>
2024-07-20 23:40:34 +0300

    4- prepared out app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express"
 import {v2 as cloudinary} from 'cloudinary';
 import dotenv from "dotenv" 
@@ -19,6 +20,8 @@ cloudinary.config({
 
 const app = express(); 
 const PORT = process.env.PORT || 5000
+const __dirname = path.resolve()
+
 app.use(express.json({limit:"5mb"})) // to parse requests
 // limit the uploaded pictures to 5mb but not TO LARGE as there some kind of attacks called DoS
 app.use(express.urlencoded({extended:true})) // to accepte the urlencoded type 
@@ -29,8 +32,23 @@ app.use("/api/users",usersRoutes)
 app.use("/api/posts",postsRoutes)
 app.use("/api/notifications",notificationsRoutes)
 
+const env = process.env.NODE_ENV ? process.env.NODE_ENV.trim() : '';
+
+if (env === "production") {
+  console.log('Entering production block');
+  
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/frontend", "dist", "index.html"));
+  });
+  console.log('NODE_ENV:', env, '__dirname:', __dirname);
+} else {
+  console.log('Not in production');
+}
 
 app.listen(PORT,()=>{
   console.log("server is running at port",PORT)
+  // console.log(path.join(__dirname,"/frontend","dist","index.html"))
   connetMongoDB()
 })
